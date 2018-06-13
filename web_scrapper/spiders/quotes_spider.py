e2619d900c4058ed249ce4dbce7216cfc0a2a22b
aminda007 <aminda.14@cse.mrt.ac.lk>
2018-06-13 10:42:22 +0530

    start scrapping srilanka.travel

--- web_scrapper/spiders/quotes_spider.py
+++ web_scrapper/spiders/quotes_spider.py
@@ -1,21 +1,41 @@
+from typing import List
+
 import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-    #
-    # def start_requests(self):
-    #     urls = [
+
     start_urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
+            'http://www.srilanka.travel/sitemap'
         ]
-        # for url in urls:
-        #     yield scrapy.Request(url=url, callback=self.parse)
+
+    scrape_list=['xxxx']
+
+    def addPage(self,page):
+        self.scrape_list.append(page)
+
+    def getPages(self):
+        return self.scrape_list
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
+        page = response.url.split("/")[-1]
+        filename = '%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
-        self.log('Saved file %s' % filename)
+        print('******************************************************************')
+
+
+        next_page_list = response.css('li a::attr(href)').extract()
+        if next_page_list is not None:
+            for page in next_page_list:
+                print(page)
+                black_list = ['youtube','twitter', 'instagram', 'wikipedia', 'facebook']
+                scrape_list = self.getPages()
+                if any(bl not in page for bl in black_list):
+                    if any(sl not in page for sl in scrape_list):
+                        self.addPage(page)
+                        print("ADDING"+page)
+                        # print(self.getPages())
+                        next_page = response.urljoin(page)
+                        yield scrapy.Request(next_page, callback=self.parse)
